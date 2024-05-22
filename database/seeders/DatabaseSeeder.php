52e46176d443f0fa12f983e48e4fca70b7a820f1
Eldos Ulanbekov <ulanbekoveldos@gmail.com>
2024-05-22 22:36:00 +0600

    create migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Console\View\Components\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +22,17 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'is_admin' => true,
         ]);
+
+        Tag::factory()->count(10)->create();
+        Post::factory()->count(50)->create();
+        Comment::factory()->count(50)->create();
+        $tags= \App\Models\Tag::all();
+        \App\Models\Post::all()->each(function ($posts) use ($tags) {
+            $posts->tags()->attach(
+                $tags->random(rand(1, 3))->pluck('id')->toArray()
+            );
+        });
     }
 }
