d3bb496f1b35a2f3e00eb360ea548a954345b9a1
Eldos Ulanbekov <ulanbekoveldos@gmail.com>
2024-05-24 00:13:14 +0600

    make documentation for Comment,Post,Tag,User

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,7 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OpenApi\Attributes as OA;
 
+#[OA\Schema(
+    schema: "Comment",
+    description: "Comment model",
+    properties: [
+        new OA\Property(property: "id", description: "ID of the comment", type: "integer"),
+        new OA\Property(property: "post_id", description: "ID of the post", type: "integer"),
+        new OA\Property(property: "content", description: "Content of the comment", type: "string"),
+    ],
+)]
 class Comment extends Model
 {
     use HasFactory;
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,7 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OpenApi\Attributes as OA;
 
+#[OA\Schema(
+    schema: "Tag",
+    description: "Tag model",
+    properties: [
+        new OA\Property(property: "id", description: "ID of the tag", type: "integer"),
+        new OA\Property(property: "name", description: "Name of the tag", type: "string"),
+    ],
+)]
 class Tag extends Model
 {
     use HasFactory;
